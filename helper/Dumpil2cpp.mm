588f0f914e3a506549305e6b3daa8a6b36fd8dd4
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-08 22:29:16 +0300

    Create Dumpil2cpp.mm

---
+++ helper/Dumpil2cpp.mm
@@ -0,0 +1,242 @@
+#import <UIKit/UIKit.h>
+#include "../IL2CPP_Resolver.hpp"
+#include <fstream>
+#include <dlfcn.h>
+#include <mach-o/dyld.h>
+
+using namespace IL2CPP;
+
+typedef void* (*DomainGet_t)();
+typedef void** (*DomainGetAssemblies_t)(void* domain, size_t* size);
+typedef void* (*AssembliesGetImage_t)(void* assembly);
+typedef const char* (*ImageGetName_t)(void* image);
+typedef int (*ImageGetClassCount_t)(void* image);
+typedef void* (*ImageGetClass_t)(void* image, int index);
+typedef void* (*ClassGetMethods_t)(void* klass, void** iter);
+
+typedef const char* (*il2cpp_class_get_name_t)(void* klass);
+typedef const char* (*il2cpp_class_get_namespace_t)(void* klass);
+typedef const char* (*il2cpp_method_get_name_t)(void* method);
+
+void showNativeAlert(NSString *title, NSString *message) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
+        
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    for (UIWindow *win in scene.windows) {
+                        if (win.isKeyWindow) {
+                            window = win;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if (!window) {
+            window = [UIApplication sharedApplication].windows.firstObject;
+        }
+        [window.rootViewController presentViewController:alert animated:YES completion:nil];
+    });
+}
+
+void shareDumpFile(NSString *filePath) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+        UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[fileURL] applicationActivities:nil];
+        
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    for (UIWindow *win in scene.windows) {
+                        if (win.isKeyWindow) {
+                            window = win;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if (!window) {
+            window = [UIApplication sharedApplication].windows.firstObject;
+        }
+        
+        UIViewController *rootVC = window.rootViewController;
+        if (!rootVC) return;
+        
+        if (activityVC.popoverPresentationController) {
+            activityVC.popoverPresentationController.sourceView = rootVC.view;
+            activityVC.popoverPresentationController.sourceRect = CGRectMake(rootVC.view.bounds.size.width / 2, rootVC.view.bounds.size.height / 2, 0, 0);
+            activityVC.popoverPresentationController.permittedArrowDirections = 0;
+        }
+        [rootVC presentViewController:activityVC animated:YES completion:nil];
+    });
+}
+
+void ExecuteIl2CppDump() {
+    @try {
+        if (!Globals.m_GameFramework) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showNativeAlert(@"Olmadı / Hata", @"Oyun modülü (Globals.m_GameFramework) yüklenmedi!");
+            });
+            return;
+        }
+
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths firstObject];
+        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"il2cpp_dump.txt"];
+        
+        std::ofstream dumpFile([filePath UTF8String], std::ios::out | std::ios::trunc);
+        if (!dumpFile.is_open()) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showNativeAlert(@"Olmadı / Kayıt Hatası", [NSString stringWithFormat:@"Documents dizinine yazılamadı:\n%@", filePath]);
+            });
+            return;
+        }
+
+        dumpFile << "=== IL2CPP Class, Method & Offset Dump ===\n\n";
+
+        DomainGet_t f_DomainGet = (DomainGet_t)Functions.m_DomainGet;
+        DomainGetAssemblies_t f_DomainGetAssemblies = (DomainGetAssemblies_t)Functions.m_DomainGetAssemblies;
+        AssembliesGetImage_t f_AssembliesGetImage = (AssembliesGetImage_t)Functions.m_AssembliesGetImage;
+        ImageGetName_t f_ImageGetName = (ImageGetName_t)Functions.m_ImageGetName;
+        ImageGetClassCount_t f_ImageGetClassCount = (ImageGetClassCount_t)Functions.m_ImageGetClassCount;
+        ImageGetClass_t f_ImageGetClass = (ImageGetClass_t)Functions.m_ImageGetClass;
+        ClassGetMethods_t f_ClassGetMethods = (ClassGetMethods_t)Functions.m_ClassGetMethods;
+
+        il2cpp_class_get_name_t f_ClassName = (il2cpp_class_get_name_t)dlsym(Globals.m_GameFramework, "il2cpp_class_get_name");
+        il2cpp_class_get_namespace_t f_ClassNamespace = (il2cpp_class_get_namespace_t)dlsym(Globals.m_GameFramework, "il2cpp_class_get_namespace");
+        il2cpp_method_get_name_t f_MethodName = (il2cpp_method_get_name_t)dlsym(Globals.m_GameFramework, "il2cpp_method_get_name");
+
+        if (!f_DomainGet || !f_DomainGetAssemblies) {
+            dumpFile.close();
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showNativeAlert(@"Olmadı / Hata", @"IL2CPP temel fonksiyonları çözülemedi.");
+            });
+            return;
+        }
+
+        void* domain = f_DomainGet();
+        size_t size = 0;
+        void** assemblies = f_DomainGetAssemblies(domain, &size);
+        if (!assemblies || size == 0) {
+            dumpFile.close();
+            dispatch_async(dispatch_get_main_queue(), ^{
+                showNativeAlert(@"Olmadı / Hata", @"Assembly listesi boş veya alınamadı!");
+            });
+            return;
+        }
+
+        int totalClasses = 0;
+        int totalMethods = 0;
+        
+        uint64_t baseAddress = 0;
+        for (uint32_t i = 0; i < _dyld_image_count(); i++) {
+            const char *imageName = _dyld_get_image_name(i);
+            if (imageName && strstr(imageName, "UnityFramework")) {
+                baseAddress = (uint64_t)_dyld_get_image_header(i);
+                break;
+            }
+        }
+        if (baseAddress == 0) {
+            baseAddress = (uint64_t)Globals.m_GameFramework;
+        }
+
+        for (size_t i = 0; i < size; ++i) {
+            void* assembly = assemblies[i];
+            if (!assembly) continue;
+
+            void* image = f_AssembliesGetImage ? f_AssembliesGetImage(assembly) : nullptr;
+            if (!image) continue;
+
+            const char* imageName = f_ImageGetName ? f_ImageGetName(image) : "Unknown";
+            int classCount = f_ImageGetClassCount ? f_ImageGetClassCount(image) : 0;
+            totalClasses += classCount;
+
+            dumpFile << "\n========================================\n";
+            dumpFile << "[Assembly: " << (imageName ? imageName : "Unknown") << "]\n";
+            dumpFile << "========================================\n";
+
+            for (int j = 0; j < classCount; ++j) {
+                void* klass = f_ImageGetClass ? f_ImageGetClass(image, j) : nullptr;
+                if (!klass) continue;
+                
+                const char* className = f_ClassName ? f_ClassName(klass) : "Unknown";
+                const char* classNamespace = f_ClassNamespace ? f_ClassNamespace(klass) : "";
+                
+                dumpFile << "\n  Class: " << (classNamespace && classNamespace[0] ? classNamespace : "") << "." << (className ? className : "Unknown") << "\n";
+
+                void* iter = nullptr;
+                while (void* method = f_ClassGetMethods ? f_ClassGetMethods(klass, &iter) : nullptr) {
+                    if (!method) continue;
+                    totalMethods++;
+
+                    const char* methodName = f_MethodName ? f_MethodName(method) : "Unknown";
+                    
+                    struct MethodInfo_Internal {
+                        void* methodPointer;
+                        void* invoker_pointer;
+                        const char* name;
+                        void* klass;
+                        void* return_type;
+                        void* parameters;
+                    };
+                    
+                    uint64_t rawPointer = (uint64_t)((MethodInfo_Internal*)method)->methodPointer;
+                    uint64_t methodPointer = rawPointer & ~((uint64_t)1);
+                    
+                    uint64_t relativeOffset = 0;
+                    if (methodPointer >= baseAddress) {
+                        relativeOffset = methodPointer - baseAddress;
+                    }
+
+                    dumpFile << "    - Method: " << (methodName ? methodName : "Unknown") 
+                             << " | Addr: 0x" << std::hex << methodPointer 
+                             << " | Offset: 0x" << relativeOffset << std::dec << "\n";
+                }
+            }
+        }
+
+        dumpFile.flush();
+        dumpFile.close();
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Oldu (Dump Başarılı)"
+                                                                            message:[NSString stringWithFormat:@"Sınıf: %d | Metot: %d\nDosya Documents dizinine oluşturuldu.", totalClasses, totalMethods]
+                                                                     preferredStyle:UIAlertControllerStyleAlert];
+            
+            [alert addAction:[UIAlertAction actionWithTitle:@"Dosyalara Kaydet" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                shareDumpFile(filePath);
+            }]];
+            
+            [alert addAction:[UIAlertAction actionWithTitle:@"Kapat" style:UIAlertActionStyleCancel handler:nil]];
+            
+            UIWindow *window = nil;
+            if (@available(iOS 13.0, *)) {
+                for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                    if (scene.activationState == UISceneActivationStateForegroundActive) {
+                        for (UIWindow *win in scene.windows) {
+                            if (win.isKeyWindow) {
+                                window = win;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (!window) {
+                window = [UIApplication sharedApplication].windows.firstObject;
+            }
+            [window.rootViewController presentViewController:alert animated:YES completion:nil];
+        });
+    }
+    @catch (NSException *exception) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            showNativeAlert(@"Olmadı / Hata", [NSString stringWithFormat:@"Exception: %@", exception.reason]);
+        });
+    }
+}
